69a2e3e677b51104159aca9f4c561eab59a4d2e7
ddavronov <65539569+ddavronov@users.noreply.github.com>
2021-03-16 19:34:06 +0500

    task_2

---
+++ урок 2/task2.js
@@ -0,0 +1,10 @@
+
+    // <!-- Построить прямоугольный треугольник состоящий из символа "*". -->
+    
+    var lines = 10;
+    var str = " ";
+    var star = "*";
+    for(var i = 0; i < lines; i++) {
+    str += star;
+    console.log(str);
+}
